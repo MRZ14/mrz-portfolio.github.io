af209ec8e14a0e413d1f7864f2f05c2c324742d8
Rai <101298615+MRaihanZ@users.noreply.github.com>
2024-08-02 20:13:29 +0700

    fix: fixing import *.css error in index.tsx and App.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
 
-import './assets/index.css'
+import './assets/css/index.css'
 import App from './App'
 
 const root = document.getElementById('root')
