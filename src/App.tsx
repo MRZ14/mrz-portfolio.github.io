e0f33c5d488aef1dc0c6cb9097a06804b81c81d2
Rai <101298615+MRaihanZ@users.noreply.github.com>
2024-08-02 20:03:07 +0700

    feat: change path to App.css in App.tsx and remove unused all import .svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './assets/css/App.css'
 
 function App() {
   const [count, setCount] = createSignal(0)
