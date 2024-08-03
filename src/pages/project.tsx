d5f96edb7e8e5cb86f688d61edf22be32fc8b7fa
Rai <101298615+MRaihanZ@users.noreply.github.com>
2024-08-03 23:45:38 +0700

    feat: adding project.tsx and detailProject.tsx files

---
+++ src/pages/project.tsx
@@ -0,0 +1,13 @@
+import MainLayout from "../layouts/main";
+
+function Project() {
+	return (
+		<>
+			<MainLayout showNavbar={false} showFooter={false}>
+				<h1>Pages Not Found (code: 404)</h1>
+			</MainLayout>
+		</>
+	);
+}
+
+export default Project;
