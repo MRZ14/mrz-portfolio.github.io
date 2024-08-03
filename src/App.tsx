8ed43ac5284f77976ebb0fabf75114fc980f80c3
Rai <101298615+MRaihanZ@users.noreply.github.com>
2024-08-03 23:44:15 +0700

    feat: adding routing for project and detail-project and import the correspondent files

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,17 @@ import "/src/assets/css/App.css"
 
 import MainLayout from "./layouts/main"
 
-const Testing = lazy(() => import("./pages/testing"));
-const NotFound = lazy(() => import("./pages/notFound"));
+const Testing = lazy(() => import("./pages/testing"))
+const NotFound = lazy(() => import("./pages/notFound"))
+const Project = lazy(() => import("./pages/project"))
+const DetailProject = lazy(() => import("./pages/detailProject"))
+
 function App() {
 	return (
 		<Router root={MainLayout}>
 			<Route path="/" component={Testing} />
+			<Route path="/project" component={Project}/>
+			<Route path="/detail-project" component={DetailProject}/>
 			<Route path="/*404" component={NotFound} />
 		</Router>
 		// <Router>
