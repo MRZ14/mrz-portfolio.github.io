3111acb87b6e6c2856cb28909684ed5e703ba320
Rai <101298615+MRaihanZ@users.noreply.github.com>
2024-08-04 21:37:48 +0700

    feat: adding component card file

---
+++ src/components/card.tsx
@@ -0,0 +1,15 @@
+import {ParentComponent, ParentProps} from "solid-js";
+
+type CardProps = ParentProps & {
+    img: string
+}
+
+const Card: ParentComponent<CardProps> = (props) => {
+    return (
+		<>
+			
+        </>
+    )
+}
+
+export default Card
