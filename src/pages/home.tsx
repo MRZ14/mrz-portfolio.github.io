b3a396896918e8766cc245ceb136282c1d8f245c
Rai <101298615+MRaihanZ@users.noreply.github.com>
2024-08-04 13:42:20 +0700

    feat: adding home.tsx page

---
+++ src/pages/home.tsx
@@ -0,0 +1,12 @@
+import MainLayout from "../layouts/main";
+function Home() {
+    return (
+		<>
+			<MainLayout showNavbar={true} showFooter={true}>
+                <p>Home pages</p>
+            </MainLayout>
+    </>
+    )
+}
+
+export default Home
