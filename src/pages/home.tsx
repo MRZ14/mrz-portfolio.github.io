e80f9650cb4e8f2ecdfe910bae5c882cfcfa9899
Rai <101298615+MRaihanZ@users.noreply.github.com>
2024-08-05 16:58:21 +0700

    feat: change structure CardComponent tag

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,14 @@
 import { useParams } from "@solidjs/router";
 import MainLayout from "../layouts/main";
+import CardComponent from "../components/card";
 function Home() {
     const lang = useParams().lang;
     return (
         <>
             <MainLayout showNavbar={true} showFooter={true} lang={lang}>
-                <p>Home pages</p>
+                <CardComponent img="test" imgAlt="test" lang={lang}>
+                    <p>Home pages</p>
+                </CardComponent>
             </MainLayout>
         </>
     );
