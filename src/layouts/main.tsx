d8fea06b94dff48033da2eb97bdb30a738e0924b
Rai <101298615+MRaihanZ@users.noreply.github.com>
2024-08-05 09:07:33 +0700

    feat: adding and passing lang props to navbar and footer

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -1,16 +1,18 @@
 import { MainLayoutProps } from "./components";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
-import {ParentComponent } from "solid-js";
+import { JSXElement, ParentComponent } from "solid-js";
 
-const MainLayout: ParentComponent<MainLayoutProps> = (props) => {
-	return (
-		<>
-			{props.showNavbar && <Navbar />}
-				<div class="main-content">{props.children}</div>
-			{props.showFooter && <Footer />}
-		</>
-	);
-}
+const MainLayout: ParentComponent<MainLayoutProps> = (props): JSXElement => {
+    return (
+        <>
+            <main class="bg-[#F5F7F8]">
+                {props.showNavbar && <Navbar lang={props.lang} />}
+                <div class="main-content">{props.children}</div>
+                {props.showFooter && <Footer lang={props.lang} />}
+            </main>
+        </>
+    );
+};
 
 export default MainLayout;
