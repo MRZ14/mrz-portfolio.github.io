dc47a65375d9cce8828f09d2b2a99dde774f76aa
Rai <101298615+MRaihanZ@users.noreply.github.com>
2024-08-05 09:02:25 +0700

    feat: adding dynamic route for choosing language

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
-import { lazy } from "solid-js"
-import { Router, Route } from "@solidjs/router"
-import "/src/assets/css/App.css"
+import { lazy } from "solid-js";
+import { Router, Route } from "@solidjs/router";
+import "/src/assets/css/App.css";
 
-const Home = lazy(() => import("./pages/home"))
-const NotFound = lazy(() => import("./pages/notFound"))
-const Project = lazy(() => import("./pages/projects/project"))
-const DetailProject = lazy(() => import("./pages/projects/detailProject"))
+const Home = lazy(() => import("./pages/home"));
+const NotFound = lazy(() => import("./pages/notFound"));
+const Project = lazy(() => import("./pages/projects/project"));
+const DetailProject = lazy(() => import("./pages/projects/detailProject"));
 
 function App() {
-	return (
-		<Router>
-			<Route path="/" component={Home} />
-			<Route path="/project">
-				<Route path="/" component={Project} />
-				<Route path="/:projectName" component={DetailProject}/>
-			</Route>
-			<Route path="/*404" component={NotFound} />
-		</Router>
-		// <Router>
-		// 	<Route path="/" component={() => <Navbar/> {Signin} <Footer/>)} />
-		// 	<Route path="/*404" component={() => {NotFound})} />
-		// </Router>
-	);
+    return (
+        <Router>
+            <Route path="/:lang">
+                <Route path="/" component={Home} />
+                <Route path="/project">
+                    <Route path="/" component={Project} />
+                    <Route path="/:projectName" component={DetailProject} />
+                </Route>
+                <Route path="/*404" component={NotFound} />
+            </Route>
+        </Router>
+        // <Router>
+        // 	<Route path="/" component={() => <Navbar/> {Signin} <Footer/>)} />
+        // 	<Route path="/*404" component={() => {NotFound})} />
+        // </Router>
+    );
 }
 
 export default App;
