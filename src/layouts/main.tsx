ddea8bb90ccc5b99f4f749185939892c09f3e4dd
Rai <101298615+MRaihanZ@users.noreply.github.com>
2024-08-05 12:35:34 +0700

    feat: changing bg color

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -6,7 +6,7 @@ import { JSXElement, ParentComponent } from "solid-js";
 const MainLayout: ParentComponent<MainLayoutProps> = (props): JSXElement => {
     return (
         <>
-            <main class="bg-[#F5F7F8]">
+            <main class="bg-[#F2F3F9]">
                 {props.showNavbar && <Navbar lang={props.lang} />}
                 <div class="main-content">{props.children}</div>
                 {props.showFooter && <Footer lang={props.lang} />}
