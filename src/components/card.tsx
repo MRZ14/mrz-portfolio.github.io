111b9e167e2acfd97ba6e0cd13b8b3e0c7757f2b
Rai <101298615+MRaihanZ@users.noreply.github.com>
2024-08-05 18:32:50 +0700

    feat: adding skeleton ui in card component

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,11 +2,21 @@ import { JSXElement, ParentComponent, ParentProps } from "solid-js";
 
 type CardProps = ParentProps & {
     img: string;
+    imgAlt: string;
     lang: string;
 };
 
 const Card: ParentComponent<CardProps> = (props): JSXElement => {
-    return <></>;
+    return (
+        <>
+            <section class="flex flex-col md:flex-row">
+                <span>
+                    <img src={props.img} alt={props.imgAlt} />
+                </span>
+                <section>{props.children}</section>
+            </section>
+        </>
+    );
 };
 
 export default Card;
