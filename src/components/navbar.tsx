bb7b60b772b6984ad45694fdeedacdfa5f5ad00b
Rai <101298615+MRaihanZ@users.noreply.github.com>
2024-08-05 12:35:55 +0700

    feat: finishing UI navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -12,20 +12,30 @@ const Navbar: Component<NavbarProps> = (props): JSXElement => {
     } else {
         return (
             <>
-                <nav id="navbar" class="md:container md:mx-auto">
-                    <section class="flex justify-around">
-                        <section>Personal Portfolio</section>
+                <nav id="navbar" class="md:container md:mx-auto md:pt-10">
+                    <section class="flex justify-around shadow-[0.4rem_0.4rem_0.4rem_0_rgb(209,213,219),-0.4rem_-0.4rem_0.4rem_0_rgb(255,255,255)] py-3 border rounded-md">
+                        <span class="flex flex-col justify-center">
+                            <A href={url()} class="text-xl">
+                                Personal Portfolio
+                            </A>
+                        </span>
                         <section class="flex">
-                            <A href={url() + "/en/#about"} class="me-5">
+                            <A
+                                href={url() + "/en/#about"}
+                                class="me-2 px-5 py-2 hover:shadow-[inset_0.2rem_0.2rem_0.4rem_0_rgb(209,213,219),inset_-0.4rem_-0.4rem_0.5rem_0_rgb(255,255,255)] rounded-md"
+                            >
                                 About
                             </A>
                             <A
                                 href={url() + "/en/#programming_language"}
-                                class="mx-5"
+                                class="mx-2 px-5 py-2 hover:shadow-[inset_0.2rem_0.2rem_0.4rem_0_rgb(209,213,219),inset_-0.4rem_-0.4rem_0.5rem_0_rgb(255,255,255)] rounded-md"
                             >
                                 Programming Language
                             </A>
-                            <A href={url() + "/en/#project"} class="ms-5">
+                            <A
+                                href={url() + "/en/#project"}
+                                class="ms-2 px-5 py-2 hover:shadow-[inset_0.2rem_0.2rem_0.4rem_0_rgb(209,213,219),inset_-0.4rem_-0.4rem_0.5rem_0_rgb(255,255,255)] rounded-md"
+                            >
                                 Project
                             </A>
                         </section>
