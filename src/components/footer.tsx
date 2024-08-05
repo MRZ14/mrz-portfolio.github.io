1f567392e8f3f2ae3afaf1f48b6363073cf2adb0
Rai <101298615+MRaihanZ@users.noreply.github.com>
2024-08-05 09:10:42 +0700

    feat: adding lang props in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,15 @@
-function Footer() {
-    return (
-    <>
-        Footer
-    </>
-    )
-}
+import { Component, JSXElement } from "solid-js";
 
-export default Footer
+type FooterProps = {
+    lang: string;
+};
+
+const Footer: Component<FooterProps> = (props): JSXElement => {
+    if (props.lang === "id") {
+        return <>Lang ID</>;
+    } else {
+        return <>Footer</>;
+    }
+};
+
+export default Footer;
